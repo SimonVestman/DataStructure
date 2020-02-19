e5fe9a2b7dcb11b07d6c5c8027a0763c440d46c6
SimonVestman <simonw202@gmail.com>
2020-02-19 13:39:21 +0100

    asd

--- src/Queue.java
+++ src/Queue.java
@@ -3,5 +3,24 @@
 * Created 2020-02-14
 */
 public class Queue {
-    
+
+    public Queue() {
+
+    }
+
+    public void enqueue(String text) {
+
+    }
+
+    public String dequeue() {
+        return "";
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return -1;
+    }
 }
